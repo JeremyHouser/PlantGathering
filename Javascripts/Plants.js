fa45020a5b46bfa153681a56cef5683f0a729161
Jeremy <foxattack3@gmail.com>
2018-06-25 12:47:11 -0500

    Update Plants.js

--- Javascripts/Plants.js
+++ Javascripts/Plants.js
@@ -1,11 +1,15 @@
 
-
+var plantsArray = [];
 $.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/plants.json", function(data){
     $.each(data, function(i, field){
         $('#ScrollbarBox').append("<a class='plantsClickable'>" + field.Name + "</a><br>");
+        var plantObject = new Object();
+        plantObject.Name = field.Name;
+        plantObject.Description = field.Description;
+        plantsArray.Add(plantObject);
     });
-    $('.plantsClickable').on( "click", function(event){
-        alert($(this).text());
-    });
-    
+});
+
+$('.plantsClickable').on( "click", function(event){
+       alert(plantsArray.find(plantOjbect.Name($(this).text())).Description);
 });
