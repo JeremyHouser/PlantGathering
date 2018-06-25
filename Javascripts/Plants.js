9507b7b4fb0abadc9a26de88b80e076a06d8f4fb
Jeremy <foxattack3@gmail.com>
2018-06-25 11:33:14 -0500

    Update Plants.js

--- Javascripts/Plants.js
+++ Javascripts/Plants.js
@@ -1,5 +1,5 @@
 $.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/plants.json", function(data){
     $.each(data.content, function(i, field){
-        $('#ScrollbarBox').append("<li>" + i + "</li>");
+        $('#ScrollbarBox').append("<li>" + field.Name + "</li>");
     });
 });
