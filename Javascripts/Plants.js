62adfbfe1d9c6f80e047a10a17c8ef9e27a9d4e8
Jeremy <foxattack3@gmail.com>
2018-06-25 13:03:08 -0500

    Update Plants.js

--- Javascripts/Plants.js
+++ Javascripts/Plants.js
@@ -1,7 +1,9 @@
 
 var plantsArray = [];
-$.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/plants.json", function(data){
-    $.each(data, function(i, field){
+$.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/plants.json", function(data)
+{
+    $.each(data, function(i, field)
+    {
         $('#ScrollbarBox').append("<a class='plantsClickable'>" + field.Name + "</a><br>");
         var plantObject = new Object();
         plantObject.Name = field.Name;
@@ -10,13 +12,17 @@ $.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/plants.json", func
     });
 });
 
-$('.plantsClickable').on( "click", function(event){
+$('.plantsClickable').on( "click", function(event)
+{
+    var desc;
     for (var i = 0, len = plantsArray.length; i < len; i++) 
     {
         if (plantsArray[i].Name === $(this).text())
         {
-            alert(plantsArray[i].Description); // Return as soon as the object is found
+            desc = plantsArray[i].Description;
+            break;
         }
     }
-}
+    
+    alert(desc);
 });
