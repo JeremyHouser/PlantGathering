cc4dbd4de5b72fde619c4518efa90ff4db497755
Jeremy <foxattack3@gmail.com>
2018-06-26 13:58:16 -0500

    Add files via upload

--- Javascripts/Plants.js
+++ Javascripts/Plants.js
@@ -1,27 +1,118 @@
 
 var plantsArray = [];
+var rarityArray = [];
+var locationArray = [];
+var plantsJson, rarityJson, locationJson;
+
 $.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/plants.json", function(data)
 {
+    plantsJson = data;
     $.each(data, function(i, field)
     {
-        $('#ScrollbarBox').append("<a class='plantsClickable'>" + field.Name + "</a><br>");
-        var plantObject = new Object();
-        plantObject.Name = field.Name;
-        plantObject.Description = field.Description;
-        plantsArray.push(plantObject);
+        $('.scrollable').append("<a class='plantsClickable'>" + field.Name + "</a><br>");
     });
 });
 
-$(document).on('click', "a.plantsClickable", function(event)
+$.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/rarities.json", function(data)
 {
-    var desc;
-    for (var i = 0, len = plantsArray.length; i < len; i++) 
-    {
-        if (plantsArray[i].Name === $(this).text())
-        {
-            $('#plantName').text(plantsArray[i].Name);
-            $('#plantInfo').text(plantsArray[i].Description);
-            break;
-        }
-    }
+    rarityJson = data;
 });
+
+$.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/locations.json", function(data)
+{
+    locationJson = data;
+});
+
+
+
+$(document).ready(function() {
+    $(document).on('click', 'a.plantsClickable', function(event)
+    {
+        var desc;
+        for (var i = 0, len = plantsJson.length; i < len; i++) 
+        {
+            if (plantsJson[i].Name === $(this).text())
+            {
+                $('#plantName').text(plantsJson[i].Name);
+                $('#plantLocation').text(FindPlantInLocation(plantsJson[i].Name) + " • ");
+                $('#plantRarity').text(FindPlantInRarity(plantsJson[i].Name));
+                $('#plantInfo').text(plantsJson[i].Description);
+                break;
+            }
+        }
+    });
+    
+    function FindPlantInRarity(plantName)
+    {    
+        for (var i = 0, len = rarityJson.length; i < len; i++)
+        {
+            for (var a = 0, leng = rarityJson[i].Plants.length; a < leng; a++)
+            {
+                if (rarityJson[i].Plants[a] == plantName)
+                {
+                    return rarityJson[i].Name;
+                }
+            }
+        } 
+    }
+
+    function FindPlantInLocation(plantName)
+    {
+        var location = "", location1 = "", location2 = "", location3 = "";
+        var locCount = 0;
+        for (var i = 0, len = locationJson.length; i < len; i++)
+        {
+            for (var a = 0, leng = locationJson[i].Plants.length; a < leng; a++)
+            {
+                if (locationJson[i].Plants[a] == plantName)
+                {
+                    ++locCount;
+                    if(locCount === 1)
+                    {
+                        location = locationJson[i].Name;
+                        location1 = locationJson[i].Name;
+                    }
+                    
+                    if(locCount === 2)
+                    {
+                        location2 = locationJson[i].Name;
+                        location = location1 + " and " + location2
+                    }
+
+                    if(locCount === 3)
+                    { 
+                        location3 = locationJson[i].Name;
+                        location = location1 + ", " + location2 + ", and " + location3;
+                    }
+                }
+            }
+        }
+
+        return location;
+    }
+    var $scrollable  = $(".scrollable"),
+    $scrollbar   = $(".scrollbar"),
+    height       = $scrollable.outerHeight(true),    // visible height
+    scrollHeight = $scrollable.prop("scrollHeight"), // total height
+    barHeight    = height * height / scrollHeight;   // Scrollbar height!
+
+    
+
+    // Scrollbar drag:
+    $scrollbar.height(barHeight).draggable( {
+        axis : "y",
+        containment : "parent", 
+        drag: function(e, ui) {
+            $scrollable.scrollTop( scrollHeight / height * ui.position.top  );
+        }
+    });
+    
+    $scrollable.on("scroll", function() {
+        $scrollbar.css({top: $scrollable.scrollTop() / height * barHeight });
+    });
+});
+
+
+
+
+// Element scroll:
