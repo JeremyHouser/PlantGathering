be6fc5df059bfbe406787dfe42d1aa73c3b8b91a
Jeremy <foxattack3@gmail.com>
2018-06-27 15:45:10 -0500

    Add files via upload

--- Javascripts/Plants.js
+++ Javascripts/Plants.js
@@ -1,15 +1,91 @@
 
-var plantsArray = [];
-var rarityArray = [];
-var locationArray = [];
 var plantsJson, rarityJson, locationJson;
+
+//only works for rarityJson and locationJson
+//or at least it will, when its FINISHED
+function populateScrollable(data, text) {
+    $.each(data, function(i, field)
+    {
+        $.each(field.Plants, function(j)
+        {
+            if (field.Name == text)
+            {
+                var containsPlant = true;
+                $(".plantsClickable").each(function() {
+                    if($.contains(field.Plants[j]))
+                    {
+                        containsPlant = false;
+                    }
+                });
+
+                if (containsPlant) 
+                {
+                    $('.scrollable').append("<li class='plantsClickable'>" + field.Plants[j] + "</li>");
+                }
+            }
+        });
+    });
+}
+
+function FindPlantInRarity(plantName, jsonToSearch)
+{    
+    var plantRar;
+    $.each(jsonToSearch, function(i, field) 
+    {
+        $.each(field.Plants, function(j, item)
+        {
+            if(item == plantName)
+            {
+                plantRar = field.Name;
+                return false;
+            }
+        });
+    });
+
+    return plantRar;
+}
+
+function FindPlantInLocation(plantName)
+{
+    var location = "", location1 = "", location2 = "", location3 = "";
+    var locCount = 0;
+    $.each(locationJson, function(i, field)
+    {
+        $.each(field.Plants, function(j, item)
+        {
+            if(item == plantName)
+            {
+                ++locCount;
+                if(locCount === 1)
+                {
+                    location = field.Name;
+                    location1 = field.Name;
+                }
+                
+                if(locCount === 2)
+                {
+                    location2 = field.Name;
+                    location = location1 + " and " + location2
+                }
+
+                if(locCount === 3)
+                { 
+                    location3 = field.Name;
+                    location = location1 + ", " + location2 + ", and " + location3;
+                }
+            }
+        });
+    });
+
+    return location;
+}
 
 $.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/plants.json", function(data)
 {
     plantsJson = data;
-    $.each(data, function(i, field)
+    $.each(plantsJson, function(i, field)
     {
-        $('.scrollable').append("<a class='plantsClickable'>" + field.Name + "</a><br>");
+        $('.scrollable').append("<li class='plantsClickable'>" + field.Name + "</li>");
     });
 });
 
@@ -23,10 +99,8 @@ $.getJSON("https://jeremyhouser.github.io/PlantGathering/JSON/locations.json", f
     locationJson = data;
 });
 
-
-
 $(document).ready(function() {
-    $(document).on('click', 'a.plantsClickable', function(event)
+    $(document).on('click', '.plantsClickable', function(event)
     {
         var desc;
         for (var i = 0, len = plantsJson.length; i < len; i++) 
@@ -35,61 +109,60 @@ $(document).ready(function() {
             {
                 $('#plantName').text(plantsJson[i].Name);
                 $('#plantLocation').text(FindPlantInLocation(plantsJson[i].Name) + " • ");
-                $('#plantRarity').text(FindPlantInRarity(plantsJson[i].Name));
+                $('#plantRarity').text(FindPlantInRarity(plantsJson[i].Name, rarityJson));
                 $('#plantInfo').text(plantsJson[i].Description);
                 break;
             }
         }
+
+        if($(this).text() == "Vortax")
+        {
+            $('#plantLocation').text("??? • ");
+        }
     });
     
-    function FindPlantInRarity(plantName)
-    {    
-        for (var i = 0, len = rarityJson.length; i < len; i++)
-        {
-            for (var a = 0, leng = rarityJson[i].Plants.length; a < leng; a++)
-            {
-                if (rarityJson[i].Plants[a] == plantName)
-                {
-                    return rarityJson[i].Name;
-                }
-            }
+    $('#plantSearchBox').keyup(function() {
+        var g = $(this).val().toLowerCase();
+        if(g == ""){
+            $('.plantsClickable').show();           
         } 
-    }
-
-    function FindPlantInLocation(plantName)
-    {
-        var location = "", location1 = "", location2 = "", location3 = "";
-        var locCount = 0;
-        for (var i = 0, len = locationJson.length; i < len; i++)
+        else
         {
-            for (var a = 0, leng = locationJson[i].Plants.length; a < leng; a++)
-            {
-                if (locationJson[i].Plants[a] == plantName)
-                {
-                    ++locCount;
-                    if(locCount === 1)
-                    {
-                        location = locationJson[i].Name;
-                        location1 = locationJson[i].Name;
-                    }
-                    
-                    if(locCount === 2)
-                    {
-                        location2 = locationJson[i].Name;
-                        location = location1 + " and " + location2
-                    }
-
-                    if(locCount === 3)
-                    { 
-                        location3 = locationJson[i].Name;
-                        location = location1 + ", " + location2 + ", and " + location3;
-                    }
-                }
-            }
+            $('li.plantsClickable').each(function(){
+                var text = $(this).text().toLowerCase();
+                (text.indexOf(g) >= 0) ? $(this).show() : $(this).hide();
+            });
         }
+    });  
+    
+    // click event that grabs name of clicked checkbox
+    // and calls populateScrollable to populate ".scrollable"
+    // with plants belonging to that rarity or environment
+    $("#divRarGrp > input:checkbox").click(function() {
+        $('.plantsClickable').remove();
 
-        return location;
-    }
+                                                                    //foreach checked checkbox, call populate, check if name of plant is already in an li, order all li alphabetically by value
+                                                                    //build here, turn into a function
+        $("input:checkbox:checked").each(function () {
+            populateScrollable(rarityJson, $(this).val());
+        });
+    });
+
+    $("#divLocGrp1 > input:checkbox").click(function() {
+        $('.plantsClickable').remove();
+
+        $("input:checkbox:checked").each(function () {
+            populateScrollable(rarityJson, $(this).val());
+        });
+    });
+
+    $("#divLocGrp2 > input:checkbox").click(function() {
+        $('.plantsClickable').remove();
+        $("input:checkbox:checked").each(function () {
+            populateScrollable(rarityJson, $(this).val());
+        });
+    });
+
     var $scrollable  = $(".scrollable"),
     $scrollbar   = $(".scrollbar"),
     height       = $scrollable.outerHeight(true),    // visible height
@@ -111,8 +184,3 @@ $(document).ready(function() {
         $scrollbar.css({top: $scrollable.scrollTop() / height * barHeight });
     });
 });
-
-
-
-
-// Element scroll:
